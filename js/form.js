300d7a4063802849a80dbd062a59e8f48d97c307
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-04-12 16:32:08 -0400

    Função para obter paciente

--- js/form.js
+++ js/form.js
@@ -5,10 +5,7 @@ botaoAdicionar.addEventListener('click', function(event) {
 
     var form = document.querySelector('#form-adiciona');
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var paciente  = obtemPaciente(form);
 
     var pacienteTr = document.createElement('tr');
 
@@ -18,11 +15,11 @@ botaoAdicionar.addEventListener('click', function(event) {
     var gorduraTd = document.createElement('td');
     var imcTd = document.createElement('td');
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
@@ -34,3 +31,16 @@ botaoAdicionar.addEventListener('click', function(event) {
 
     tabela.appendChild(pacienteTr);
 });
+
+function obtemPaciente(form) {
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(peso, altura) 
+    };
+
+    return paciente;
+}
