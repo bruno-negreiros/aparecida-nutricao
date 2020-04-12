a968e3c2bb5d77b389e5b9531a29c021327f20c9
= <=>
2020-04-11 21:33:07 -0400

    Alterando o estilo da linha do paciente que tiver erro na validação

--- js/principal.js
+++ js/principal.js
@@ -20,11 +20,13 @@ for(var i = 0; i < pacientes.length; i ++) {
     if(peso <= 0 || peso >= 1000) {
         pesoEhValido = false;
         tdPeso.textContent = 'Peso Inválido';
+        paciente.classList.add('paciente-invalido');
     }
 
     if(altura <= 0 || altura >= 3.00) {
         alturaEhValida = false;
         tdAltura.textContent = 'Altura Inválida';
+        paciente.classList.add('paciente-invalido');
     }
 
 
