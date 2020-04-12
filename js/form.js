894e4fce804dfabb780d5ae60e7ccf50252f9a50
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-04-12 16:50:06 -0400

    Função para criar td

--- js/form.js
+++ js/form.js
@@ -4,47 +4,44 @@ botaoAdicionar.addEventListener('click', function(event) {
     event.preventDefault();
 
     var form = document.querySelector('#form-adiciona');
-    var paciente  = obtemPaciente(form);
-    var pacienteTr = criaTr(paciente);
+    var paciente  = obtemPacienteDoFormulario(form);
+    var pacienteTr = montaTr(paciente);
 
     var tabela = document.querySelector('#tabela-pacientes');
 
     tabela.appendChild(pacienteTr); 
 });
 
-function obtemPaciente(form) {
+function obtemPacienteDoFormulario(form) {
     var paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calculaImc(peso, altura) 
+        imc: calculaImc(form.peso.value, form.altura.value) 
     };
 
     return paciente;
 }
 
-function criaTr(paciente) {
+function montaTr(paciente) {
     var pacienteTr = document.createElement('tr');
     pacienteTr.classList.add('paciente');
 
-    var nomeTd = document.createElement('td');
-    var pesoTd = document.createElement('td');
-    var alturaTd = document.createElement('td');
-    var gorduraTd = document.createElement('td');
-    var imcTd = document.createElement('td');
-
-    nomeTd.textContent = paciente.nome;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'));
+    pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'));
+    pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'));
+    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'));
+    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'));
 
     return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement('td');
+    
+    td.classList.add(classe);
+    td.textContent = dado;
+    
+    return td;
 }
