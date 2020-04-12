2e5a198aa6c96c02f20a7ec2f84320eb98d78a19
= <=>
2020-04-11 20:02:29 -0400

    Variáveis e operadores

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,32 @@
 var title = document.querySelector('.title');
 title.textContent = 'Aparecida Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+var tdPeso = paciente.querySelector('.info-peso');
+var tdAltura = paciente.querySelector('.info-altura');
+var tdImc = paciente.querySelector('.info-imc');
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+if(peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdPeso.textContent = 'Peso Inválido';
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    alturaEhValida = false;
+    tdAltura.textContent = 'Altura Inválida';
+}
+
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = 'Altura e/ou peso inválidos!';
+}
+
