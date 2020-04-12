da91733a2dc80b99b398429f607404bb3f201f5a
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-04-12 19:02:50 -0400

    Função para validação de peso e altura

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -7,9 +7,6 @@ for(var i = 0; i < pacientes.length; i ++) {
 
     var paciente = pacientes[i];
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
-
     var tdPeso = paciente.querySelector('.info-peso');
     var tdAltura = paciente.querySelector('.info-altura');
     var tdImc = paciente.querySelector('.info-imc');
@@ -17,13 +14,16 @@ for(var i = 0; i < pacientes.length; i ++) {
     var peso = tdPeso.textContent;
     var altura = tdAltura.textContent;
 
-    if(peso <= 0 || peso >= 1000) {
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+
+    if(!pesoEhValido) {
         pesoEhValido = false;
         tdPeso.textContent = 'Peso Inválido';
         paciente.classList.add('paciente-invalido');
     }
 
-    if(altura <= 0 || altura >= 3.00) {
+    if(!alturaEhValida) {
         alturaEhValida = false;
         tdAltura.textContent = 'Altura Inválida';
         paciente.classList.add('paciente-invalido');
@@ -37,6 +37,23 @@ for(var i = 0; i < pacientes.length; i ++) {
         tdImc.textContent = 'Altura e/ou peso inválidos!';
     }
 }
+
+function validaPeso(peso) {
+    if(peso <= 0 || peso >= 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validaAltura(altura) {
+    if(altura <= 0 || altura >= 3.00) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 
 function calculaImc(peso, altura) {
     var imc = peso / (altura * altura);
